54a033daf6c4ff00f693b2ce64ecd59b1972c2d8
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-02-12 16:46:01 +0100

    state

---
+++ state/index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>State</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-center">Repositories (milivojevicentijevic)</h1>
+        <h4 class="text-center">MySQL user: root</h4>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>rep. name</th>
+                    <th>rep. folder</th>
+                    <th>database</th>
+                    <th>group</th>
+                    <th>description</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>State</td>
+                    <td>state</td>
+                    <td>-</td>
+                    <td>info</td>
+                    <td>info</td>
+                </tr>
+                <tr>
+                    <td rowspan="3">cwd_php</td>
+                    <td>OOP_Course</td>
+                    <td>-</td>
+                    <td>oop php</td>
+                    <td>oop</td>
+                </tr>
+                <tr>
+                    <td>PHP_Course</td>
+                    <td>-</td>
+                    <td>php basics</td>
+                    <td>php basics</td>
+                </tr>
+                <tr>
+                    <td>mvcframework</td>
+                    <td>mvcframework</td>
+                    <td>php mvc</td>
+                    <td>mvc framework</td>
+                </tr>
+                <tr>
+                    <td rowspan="2">cwd</td>
+                    <td>HTML&CSS3</td>
+                    <td>-</td>
+                    <td>html</td>
+                    <td>html and css</td>
+                </tr>
+                <tr>
+                    <td>JSFundamentals</td>
+                    <td>-</td>
+                    <td>js</td>
+                    <td>js fundamentals</td>
+                </tr>
+                <tr>
+                    <td>tnn</td>
+                    <td>OOPHP</td>
+                    <td>-</td>
+                    <td>oop</td>
+                    <td>oop</td>
+                </tr>
+                <tr>
+                    <td>bitfumes</td>
+                    <td>Bitfumes_OOPPHP</td>
+                    <td>-</td>
+                    <td>oop</td>
+                    <td>oop</td>
+                </tr>
+                <tr>
+                    <td>GaryClarke</td>
+                    <td>Learn_OOPHP</td>
+                    <td>-</td>
+                    <td>oop</td>
+                    <td>oop</td>
+                </tr>
+                <tr>
+                    <td>peterfisher</td>
+                    <td>PHPoopBeginners</td>
+                    <td>-</td>
+                    <td>oop</td>
+                    <td>oop</td>
+                </tr>
+                <tr>
+                    <td>CleverTechie</td>
+                    <td>CRUDwBstrap</td>
+                    <td>ct_crud</td>
+                    <td>crud</td>
+                    <td>crud</td>
+                </tr>
+                <tr>
+                    <td>pshoots</td>
+                    <td>crud</td>
+                    <td>pshoots</td>
+                    <td>crud</td>
+                    <td>crud</td>
+                </tr>
+                <tr>
+                    <td>techbowl</td>
+                    <td>techbowl_crud</td>
+                    <td>techbowl</td>
+                    <td>crud</td>
+                    <td>crud</td>
+                </tr>
+                <tr>
+                    <td>veryacademy</td>
+                    <td>PHP-CRUD</td>
+                    <td>cms</td>
+                    <td>crud</td>
+                    <td>crud</td>
+                </tr>
+                <tr>
+                    <td>codeandcoins_crud</td>
+                    <td>crud</td>
+                    <td>cac_crud</td>
+                    <td>crud</td>
+                    <td>crud</td>
+                </tr>
+                <tr>
+                    <td>awemelvine</td>
+                    <td>php_crud</td>
+                    <td>awecrud</td>
+                    <td>crud</td>
+                    <td>crud</td>
+                </tr>
+                <tr>
+                    <td>tttCrud</td>
+                    <td>tttCrud</td>
+                    <td>tttcrud</td>
+                    <td>crud</td>
+                    <td>crud</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
